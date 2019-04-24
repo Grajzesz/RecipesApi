2f3f4da839c8452696c885c29c83983ccbe50859
Mariusz Buskiewicz <grajzesz@gmail.com>
2019-04-24 11:06:41 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Form from './Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save maruiusz to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ApiKey = '67d51b152c762ab5dd5a32213b7fd4dd';
+class App extends Component {
+    state = {
+        desc: '',
+        value: ''
+    };
+
+    handleTextChange = e => {
+        this.setState({
+            value: e.target.value
+        });
+    };
+    handleCitySubmit = e => {
+        e.preventDefault();
+        console.log('lala');
+
+        const Api = `https://api.edamam.com/search?q=${
+            this.state.value
+        }&app_id=f7495c5e&app_key=${ApiKey}&to=20`;
+
+        fetch(Api)
+            .then(response => {
+                if (response.ok) {
+                    return response;
+                }
+                throw Error('lipa');
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                this.setState(prevState => ({
+                    err: false,
+
+                    city: prevState.value
+                }));
+            });
+    };
+
+    render() {
+        return (
+            <div>
+                <Form
+                    value={this.state.value}
+                    change={this.handleTextChange}
+                    submit={this.handleCitySubmit}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Form = props => {
+    // let valiue = document.querySelector("value")
+    // let po = valiue.toUpperCase()
+    return (
+        <div className="formularz">
+            <form className="forms" onSubmit = {props.submit}>
+                <input
+                    type="text"
+                    value={props.value}
+                    onChange={props.change}
+                    placeholder={'Ingredient'}
+                />
+
+                <button>Search Ingredients</button>
+            </form>
+        </div>
+    );
+};
+export default Form;
