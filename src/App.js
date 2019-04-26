6e58c8ca4a6271db7fc5f8012ba2bade8e172e64
Mariusz Buskiewicz <grajzesz@gmail.com>
2019-04-26 11:17:36 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -22,6 +22,9 @@ class App extends Component {
         console.log('lala');
         var counter = -1;
         function add() {
+            if(counter === 19){
+                counter=-1
+            }
             return (counter += 1);
         }
         var counter2 = -1;
@@ -71,29 +74,50 @@ class App extends Component {
                     ],
                     labels: [
                         {
-                            label0: data.hits[add2()].recipe.label,
-                            label1: data.hits[add2()].recipe.label,
-                            label2: data.hits[add2()].recipe.label,
-                            label3: data.hits[add2()].recipe.label,
-                            label4: data.hits[add2()].recipe.label,
-                            label5: data.hits[add2()].recipe.label,
-                            label6: data.hits[add2()].recipe.label,
-                            label7: data.hits[add2()].recipe.label,
-                            label8: data.hits[add2()].recipe.label,
-                            label9: data.hits[add2()].recipe.label,
-                            label10: data.hits[add2()].recipe.label,
-                            label11: data.hits[add2()].recipe.label,
-                            label12: data.hits[add2()].recipe.label,
-                            label13: data.hits[add2()].recipe.label,
-                            label14: data.hits[add2()].recipe.label,
-                            label15: data.hits[add2()].recipe.label,
-                            label16: data.hits[add2()].recipe.label,
-                            label17: data.hits[add2()].recipe.label,
-                            label18: data.hits[add2()].recipe.label,
-                            label19: data.hits[add2()].recipe.label
+                            label0: data.hits[add()].recipe.label,
+                            label1: data.hits[add()].recipe.label,
+                            label2: data.hits[add()].recipe.label,
+                            label3: data.hits[add()].recipe.label,
+                            label4: data.hits[add()].recipe.label,
+                            label5: data.hits[add()].recipe.label,
+                            label6: data.hits[add()].recipe.label,
+                            label7: data.hits[add()].recipe.label,
+                            label8: data.hits[add()].recipe.label,
+                            label9: data.hits[add()].recipe.label,
+                            label10: data.hits[add()].recipe.label,
+                            label11: data.hits[add()].recipe.label,
+                            label12: data.hits[add()].recipe.label,
+                            label13: data.hits[add()].recipe.label,
+                            label14: data.hits[add()].recipe.label,
+                            label15: data.hits[add()].recipe.label,
+                            label16: data.hits[add()].recipe.label,
+                            label17: data.hits[add()].recipe.label,
+                            label18: data.hits[add()].recipe.label,
+                            label19: data.hits[add()].recipe.label
                         }
-                    ]
-
+                    ],
+ingredients:[{
+    ingredients0:data.hits[add()].recipe.ingredientLines,
+    ingredients1:data.hits[add()].recipe.ingredientLines,
+    ingredients2:data.hits[add()].recipe.ingredientLines,
+    ingredients3:data.hits[add()].recipe.ingredientLines,
+    ingredients4:data.hits[add()].recipe.ingredientLines,
+    ingredients5:data.hits[add()].recipe.ingredientLines,
+    ingredients6:data.hits[add()].recipe.ingredientLines,
+    ingredients7:data.hits[add()].recipe.ingredientLines,
+    ingredients8:data.hits[add()].recipe.ingredientLines,
+    ingredients9:data.hits[add()].recipe.ingredientLines,
+    ingredients10:data.hits[add()].recipe.ingredientLines,
+    ingredients11:data.hits[add()].recipe.ingredientLines,
+    ingredients12:data.hits[add()].recipe.ingredientLines,
+    ingredients13:data.hits[add()].recipe.ingredientLines,
+    ingredients14:data.hits[add()].recipe.ingredientLines,
+    ingredients15:data.hits[add()].recipe.ingredientLines,
+    ingredients16:data.hits[add()].recipe.ingredientLines,
+    ingredients17:data.hits[add()].recipe.ingredientLines,
+    ingredients18:data.hits[add()].recipe.ingredientLines,
+    ingredients19:data.hits[add()].recipe.ingredientLines,
+}]
                     // image: prevState.image + 1 + data.hits[add()].recipe.image
                 }));
                 console.log(this.state.images);
--- src/Result.js
+++ src/Result.js
@@ -1,95 +1,117 @@
 import React from 'react';
 
 const Result = props => {
-    const {
-        err,images,labels
+    const { err, images, labels, ingredients } = props.recipe;
 
-    } = props.recipe;
-
-
-    // console.log(props.recipe.labels[0].label0);
-   let content = null;
+    // console.log(props.recipe.ingredients[0]);
+    let content = null;
     if (err === false) {
         content = (
             <div className="content">
-            <div className="food__container">
-                <img src={images[0].image0} alt="djis" />
-                <h3>{labels[0].label0}</h3>
+                <div className="food__container">
+                    <img src={images[0].image0} alt="djis" />
+                    <h3>{labels[0].label0}</h3>
+                    <p>{ingredients[0].ingredients0.join(', \r\n')}</p>
                 </div>
                 <div className="food__container">
-                <img src={images[0].image1} alt="djis" /><h3>{labels[0].label1}</h3>
+                    <img src={images[0].image1} alt="djis" />
+                    <h3>{labels[0].label1}</h3>
+                    <p>{ingredients[0].ingredients1.join(', \r\n')}</p>
                 </div>
                 <div className="food__container">
-                <img src={images[0].image2} alt="djis" /><h3>{labels[0].label2}</h3>
+                    <img src={images[0].image2} alt="djis" />
+                    <h3>{labels[0].label2}</h3>
+                    <p>{ingredients[0].ingredients2.join(', \r\n')}</p>
                 </div>
                 <div className="food__container">
-                <img src={images[0].image3} alt="djis" /><h3>{labels[0].label3}</h3>
+                    <img src={images[0].image3} alt="djis" />
+                    <h3>{labels[0].label3}</h3>
+                    <p>{ingredients[0].ingredients3.join(', \r\n')}</p>
                 </div>
                 <div className="food__container">
-                <img src={images[0].image4} alt="djis" /><h3>{labels[0].label4}</h3>
+                    <img src={images[0].image4} alt="djis" />
+                    <h3>{labels[0].label4}</h3>
+                    <p>{ingredients[0].ingredients4.join(', \r\n')}</p>
                 </div>
                 <div className="food__container">
-                <img src={images[0].image5} alt="djis" /><h3>{labels[0].label5}</h3>
-</div>
+                    <img src={images[0].image5} alt="djis" />
+                    <h3>{labels[0].label5}</h3>
+                    <p>{ingredients[0].ingredients5.join(', \r\n')}</p>
+                </div>
                 <div className="food__container">
-                <img src={images[0].image6} alt="djis" /><h3>{labels[0].label6}</h3>
-</div>
+                    <img src={images[0].image6} alt="djis" />
+                    <h3>{labels[0].label6}</h3>
+                    <p>{ingredients[0].ingredients6.join(', \r\n')}</p>
+                </div>
                 <div className="food__container">
-
-                <img src={images[0].image7} alt="djis" /><h3>{labels[0].label7}</h3>
-</div>
+                    <img src={images[0].image7} alt="djis" />
+                    <h3>{labels[0].label7}</h3>
+                    <p>{ingredients[0].ingredients7.join(', \r\n')}</p>
+                </div>
                 <div className="food__container">
-
-
-                <img src={images[0].image8} alt="djis" /><h3>{labels[0].label8}</h3>
-</div>
+                    <img src={images[0].image8} alt="djis" />
+                    <h3>{labels[0].label8}</h3>
+                    <p>{ingredients[0].ingredients8.join(', \r\n')}</p>
+                </div>
                 <div className="food__container">
-
-
-                <img src={images[0].image9} alt="djis" /><h3>{labels[0].label9}</h3>
-</div>
+                    <img src={images[0].image9} alt="djis" />
+                    <h3>{labels[0].label9}</h3>
+                    <p>{ingredients[0].ingredients9.join(', \r\n')}</p>
+                </div>
                 <div className="food__container">
-
-                <img src={images[0].image10} alt="djis" /><h3>{labels[0].label10}</h3>
-</div>
+                    <img src={images[0].image10} alt="djis" />
+                    <h3>{labels[0].label10}</h3>
+                    <p>{ingredients[0].ingredients10.join(', \r\n')}</p>
+                </div>
                 <div className="food__container">
-
-                <img src={images[0].image11} alt="djis" /><h3>{labels[0].label11}</h3>
-</div>
+                    <img src={images[0].image11} alt="djis" />
+                    <h3>{labels[0].label11}</h3>
+                    <p>{ingredients[0].ingredients11.join(', \r\n ')}</p>
+                </div>
                 <div className="food__container">
-
-
-                <img src={images[0].image12} alt="djis" /><h3>{labels[0].label12}</h3>
-</div>
-
+                    <img src={images[0].image12} alt="djis" />
+                    <h3>{labels[0].label12}</h3>
+                    <p>{ingredients[0].ingredients12.join(', \r\n')}</p>
+                </div>
 
                 <div className="food__container">
-
-
-                <img src={images[0].image13} alt="djis" /><h3>{labels[0].label13}</h3>
-</div>
+                    <img src={images[0].image13} alt="djis" />
+                    <h3>{labels[0].label13}</h3>
+                    <p>{ingredients[0].ingredients13.join(', \r\n')}</p>
+                </div>
                 <div className="food__container">
-                <img src={images[0].image14} alt="djis" /><h3>{labels[0].label14}</h3>
-</div>
+                    <img src={images[0].image14} alt="djis" />
+                    <h3>{labels[0].label14}</h3>
+                    <p>{ingredients[0].ingredients14.join(', \r\n')}</p>
+                </div>
                 <div className="food__container">
-                <img src={images[0].image15} alt="djis" /><h3>{labels[0].label15}</h3>
-</div>
+                    <img src={images[0].image15} alt="djis" />
+                    <h3>{labels[0].label15}</h3>
+                    <p>{ingredients[0].ingredients15.join(', \r\n')}</p>
+                </div>
                 <div className="food__container">
-                <img src={images[0].image16} alt="djis" /><h3>{labels[0].label16}</h3>
-</div>
+                    <img src={images[0].image16} alt="djis" />
+                    <h3>{labels[0].label16}</h3>
+                    <p>{ingredients[0].ingredients16.join(', \r\n')}</p>
+                </div>
                 <div className="food__container">
-                <img src={images[0].image17} alt="djis" /><h3>{labels[0].label17}</h3>
-</div>
+                    <img src={images[0].image17} alt="djis" />
+                    <h3>{labels[0].label17}</h3>
+                    <p>{ingredients[0].ingredients17.join(', \r\n')}</p>
+                </div>
                 <div className="food__container">
-                <img src={images[0].image18} alt="djis" /><h3>{labels[0].label18}</h3>
-</div>
+                    <img src={images[0].image18} alt="djis" />
+                    <h3>{labels[0].label18}</h3>
+                    <p>{ingredients[0].ingredients18.join(', \r\n')}</p>
+                </div>
                 <div className="food__container">
-                <img src={images[0].image19} alt="djis" /><h3>{labels[0].label19}</h3>
-</div>
-
+                    <img src={images[0].image19} alt="djis" />
+                    <h3>{labels[0].label19}</h3>
+                    <p>{ingredients[0].ingredients19.join(', \r\n')}</p>
+                </div>
             </div>
         );
     }
-    return <div className="result">{content}</div> ;
+    return <div className="result">{content}</div>;
 };
 export default Result;
