8416435045fa4b98d16cf493eb25b834aef778be
Mariusz Buskiewicz <grajzesz@gmail.com>
2020-01-29 23:21:43 +0100

    Updates

---
+++ precache-manifest.7768738c9442d03b8aa9a5b822fac74d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed739fb5e9e609776ee7fb9e4b3140a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a118342d30cc58d00c13",
+    "url": "/static/css/main.5fa9cc96.chunk.css"
+  },
+  {
+    "revision": "ffbe32d4e790e6a5d2f2",
+    "url": "/static/js/2.335f0c66.chunk.js"
+  },
+  {
+    "revision": "a118342d30cc58d00c13",
+    "url": "/static/js/main.de830940.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
